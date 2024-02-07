a5b05415cbb048f4dc49b44db39852fca10d0e4b
joao.mateus <joao.mateus@softplan.com.br>
2024-02-07 08:22:12 -0300

    Get Config 2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 
@@ -12,17 +11,3 @@
 // }
 
 // module.exports = nextConfig
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/uploads/:path*',
-        destination: 'https://nextnewsproject.s3.sa-east-1.amazonaws.com/news-images/:path*' // Direciona diretamente para o bucket do S3
-      }]
-  }
-}
-
-module.exports = nextConfig;
->>>>>>> 1c74cadd0c5eb4434f5c9d0b607809b60e746cc6
